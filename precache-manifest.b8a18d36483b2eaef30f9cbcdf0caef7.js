140d2ea0eb3a10ff29767540041d8ef7b2801971
Andrew <andrewmfellows@blueyonder.co.uk>
2020-04-07 15:44:47 +0100

    Updates

---
+++ precache-manifest.b8a18d36483b2eaef30f9cbcdf0caef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320e5d55cb29aa63f5bc9ffc5fca410c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c534db2a4c9886aed356",
+    "url": "/monsters-rolodex/static/css/main.e8dd5da9.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c534db2a4c9886aed356",
+    "url": "/monsters-rolodex/static/js/main.3b8865dc.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
